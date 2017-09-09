6fb8ec7ff5c55dacc8075dc6fb666d4b7850c49c
Ryuu Sakurai <sakurai.ryuu@gmail.com>
2017-09-09 16:54:54 +0200

    connect

---
+++ UnityClient-Example/Assets/_Script/Menager/GameMenager.cs
@@ -0,0 +1,32 @@
+﻿using ClientSocket;
+using UnityEngine;
+
+public class GameMenager : MonoBehaviour
+{
+    [SerializeField]
+    private string ip;
+
+    [SerializeField]
+    private short port;
+
+
+    private SocketClient socket;
+
+
+    private void Awake()
+    {
+        socket = new SocketClient(ip, port);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ UnityClient-Example/Assets/_Script/Networking/SocketClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ClientSocket
+{
+    public class SocketClient
+    {
+        private Socket socket;
+
+        private string ip;
+        private short port;
+
+        public SocketClient()
+        {
+
+        }
+
+        public SocketClient(string ip, short port)
+        {
+            this.ip = ip;
+            this.port = port;
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            socket.Connect(endPoint);
+        }
+
+        public void Run()
+        {
+
+        }
+
+
+        public Socket Soscket
+        {
+            get { return socket; }
+        }
+    }
+}
